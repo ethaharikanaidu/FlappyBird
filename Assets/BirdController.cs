605b6e5cbc55024a62d447aff4cd6eece8f0cb72
Harika Etha <ethaharikanaidu2215@gmail.com>
2023-06-17 16:07:13 -0400

    New Features v2

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -1,28 +1,45 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class BirdController : MonoBehaviour
 {
     // Creating variable and giving it a name 
     public Rigidbody2D myrigidbody;
-    //  
-    public float Ystrength = 10 ;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+    //
+    public InputField Speed, gravity;
+    public int Ystrength = 10 ;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+    private void OnEnable()
+    {
+        // parse converting string to float.
+        Ystrength = int.Parse(Speed.text);
+        // 
+        Physics2D.gravity = new Vector2(0, float.Parse(gravity.text)); 
     }
 
     // Update is called once per frame
     void Update()
     {
         // To tell the system if I press Space apply the body statement
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        // && means both conditions should be true.
+        // || means any of conidtions is true it will work.
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive == true)
         {
             //New Vector2(0,1);
             myrigidbody.velocity = Vector2.up * Ystrength;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
 
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
 
@@ -16,5 +18,12 @@ public class LogicScript : MonoBehaviour
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
     }
-   
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
 }
